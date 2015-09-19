5fc1a0e0588a5ce0b55c2e26b4c6a9f4a58418bf
Gharbi Mohammed <simo163@hotmail.co.uk>
2015-09-19 13:44:56 +0200

    using the user route as route base object

--- app/routes/user.js
+++ app/routes/user.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
+import PaginationBaseRoute from './pagination-base';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(PaginationBaseRoute, {
+	limit: 5,
+	
+	init() {
+		this._super('model');
+	},
+	
+	model(params) {
+		var model = this._super(params);
+		model.get('lastObject').set('lastName', 'Rogger updated');
+		return model;
+	}
 });
