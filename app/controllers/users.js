cbae27f5eba2b78269a05a74a79ff768db0941eb
Gharbi Mohammed <simo163@hotmail.co.uk>
2015-09-19 15:14:28 +0200

    creating users controller as paginated controller

---
+++ app/controllers/users.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+import Paginated from '../mixins/paginated';
+
+export default Ember.Controller.extend(Paginated, {
+	
+	total: Ember.computed('model', function(){
+		// return this.get('model.meta').total;
+		return this.get('model.length');
+	})
+});
