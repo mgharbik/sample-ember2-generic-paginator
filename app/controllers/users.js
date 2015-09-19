4bff0daef8674cf3978cb73457c07d45694f1562
Gharbi Mohammed <simo163@hotmail.co.uk>
2015-09-19 16:28:48 +0200

    creating the pagination-base component

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -3,7 +3,7 @@ import Paginated from '../mixins/paginated';
 
 export default Ember.Controller.extend(Paginated, {
 	
-	total: Ember.computed('model', function(){
+	total: Ember.computed('model', function() {
 		// return this.get('model.meta').total;
 		return this.get('model.length');
 	})
